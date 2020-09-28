625ac030d385ca60b756c139d0bbfe0214fb40cf
xiangli19930605 <xiangli19938748>
2020-09-28 20:39:23 +0800

    2020/9/28

--- module_main/src/main/java/com/idealbank/module_main/app/rfid_module/ONDEVMESSAGE.java
+++ module_main/src/main/java/com/idealbank/module_main/app/rfid_module/ONDEVMESSAGE.java
@@ -26,36 +26,35 @@ public class ONDEVMESSAGE implements rfid_interface.OnReadMessage {
             if (action.equals("inventory")) {
                 String originTagid = json_data.getString("tagid");//初始的ID
                 String tagid;
-                String  type;//类型 fe:院内资产 ff:车辆标签 fc:院外资产
-                int   tagType  ;//类型  0123
+                String type;//类型 fe:院内资产 ff:车辆标签 fc:院外资产 研究院内涉密资产标签 = "fe";研究院内普通资产标签= "fd"; 外来物品电子标签 = "fc"; 电子车标 "ff";
+                int tagType;//类型  0123
                 int str;//实际数据长度
                 try {
-                    type=originTagid.substring(0, 2);
+                    type = originTagid.substring(0, 2);
                     //过滤掉其他标签  留下FE FC
-                    if(type.equals("FE")){
-                        tagType=0;
-                    }else if(type.equals("FC")){
-                        tagType=2;
-                    }else{
+                    if (type.equals("FE")) {
+                        tagType = 0;
+                    } else if (type.equals("FC")) {
+                        tagType = 2;
+                    } else {
                         return;
                     }
                     str = Integer.valueOf(originTagid.substring(2, 4));//截取前两个字符
-                }catch (NumberFormatException e){
-                    LogUtils.e(""+e);
+                } catch (NumberFormatException e) {
+                    LogUtils.e("" + e);
                     return;
                 }
                 try {
-                    tagid = originTagid.substring(4,  4+str);
-                }catch (StringIndexOutOfBoundsException e){
-                    LogUtils.e(""+e);
+                    tagid = originTagid.substring(4, 4 + str);
+                } catch (StringIndexOutOfBoundsException e) {
+                    LogUtils.e("" + e);
                     return;
                 }
-                EventBusUtils.sendEvent(new Event(new RfidData(tagid,tagType) ), EventBusTags.SCANRFID);
+                EventBusUtils.sendEvent(new Event(new RfidData(tagid, tagType)), EventBusTags.SCANRFID);
             } else if (action.equals("readtag")) {
                 // 读取标签
                 // {"action":"readtag", "error":"0", "tagid":"A09000000000C509", "data":"0000000000000000"}
                 EventBusUtils.sendEvent(new Event("", szCode), EventBusTags.READTAG);
-
             } else if (action.equals("writetag")) {
                 EventBusUtils.sendEvent(new Event("", szCode), EventBusTags.WRITETAG);
                 json_data.getString("error");
@@ -66,7 +65,6 @@ public class ONDEVMESSAGE implements rfid_interface.OnReadMessage {
                     System.out.println(json_data.getString("error"));
                     ToastUtil.showToast(json_data.getString("error"));
                 }
-
             }
 
         } catch (JSONException e) {
